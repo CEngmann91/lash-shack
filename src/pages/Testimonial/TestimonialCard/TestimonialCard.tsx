9642c144672a5652dbfba7f07e71d456eed8deb9
CEngmann91 <christian.j.engmann@googlemail.com>
2022-11-24 23:30:19 +0000

    minor

--- src/pages/Testimonial/TestimonialCard/TestimonialCard.tsx
+++ src/pages/Testimonial/TestimonialCard/TestimonialCard.tsx
@@ -22,7 +22,7 @@ const TestimonialCard: React.FC<iTestimonialReview> = ({ id, createdAt, starRati
 
     return (
         <article className="testimonial-card">
-            <AnimatePresence>
+            {/* <AnimatePresence> */}
                 <motion.div
                     className="testimonial-card--wrapper"
                     variants={variants}
@@ -60,56 +60,7 @@ const TestimonialCard: React.FC<iTestimonialReview> = ({ id, createdAt, starRati
                         </div>
                     </div>
                 </motion.div>
-
-
-
-
-
-
-                {/* <motion.div
-                    className="testimonial-card--wrapper"
-                    initial={{ rotateY: 0 }}
-                    viewport={{ once: true }}
-                    whileInView={{
-                        rotateY: 180,
-                        transition: {
-                            duration: 1,
-                            delay: id * 0.1,
-                            ease: 'easeOut'
-                        }
-                    }}
-                    whileHover={{
-                        scale: 1.05,
-                        boxShadow: "0 5px 15px rgba(0, 0, 0, 0.2)",
-                        transition: {
-                            duration: 0.001,
-                            // ease: 'linear'
-                        }
-                    }}
-                >
-                    <div className="back">
-                        <div className="back--star">
-                            {Array(starRating).fill(1).map((el, i) => <Star key={i} />)}
-                        </div>
-
-                        <div className="back--title">
-                            <h1>{title}</h1>
-
-                            <span className="quote-icon">
-                                <QuoteRight />
-                            </span>
-                        </div>
-
-                        <div className="back--description">
-                            <label>{description}</label>
-                        </div>
-
-                        <div className="back--footer">
-                            <label className="text">~~ {getDate(createdAt)}</label>
-                        </div>
-                    </div>
-                </motion.div> */}
-            </AnimatePresence>
+            {/* </AnimatePresence> */}
         </article>
     )
 }
