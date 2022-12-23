ac77729042272d9035a7c343109f63ac30869bec
CEngmann91 <christian.j.engmann@googlemail.com>
2022-12-23 16:48:07 +0000

    minor

--- src/pages/Main/Courses/Certificate/Certificate.tsx
+++ src/pages/Main/Courses/Certificate/Certificate.tsx
@@ -7,13 +7,18 @@ const Certificate = () => {
     
   return (
     <Page id='certificate' className='app__flex app__certificate' header='What Happens Next?'>
-      <p>At the end of every training, we present the newly qualified trainee with this certficate.</p>
-      <p>We provide ongoing support, provide shadow days and ongoing assistance with models, if needed.</p>
+      <p className='txt-cntr'>
+        At the end of every training, we present the newly qualified trainee with this certficate.
+        We provide ongoing support, provide shadow days and ongoing assistance with models, if needed.
+      </p>
+
       <div className="app__certificate--img">
         <img src={TrainingCertificate_Watermark} className='' />
       </div>
 
-      <img className='abt' src={ABT} alt="" />
+      <div className="abt">
+        <img src={ABT} alt="" />
+      </div>
     </Page>
   )
 }
