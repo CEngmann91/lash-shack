6453d12e9ef316d95caa56e34b63e7ecfc80e6e5
CEngmann91 <christian.j.engmann@googlemail.com>
2023-01-19 22:02:32 +0000

    maintenance

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbar from '../Navbar/Navbar'
+import Navbar from '../navbar/Navbar'
 import Footer from '../Footer/Footer'
 import Routers from '../../routers/Routers'
 
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,4 @@
-export { default as Navbar } from './Navbar/Navbar';
+export { default as Navbar } from './navbar/Navbar';
 export { default as DrawerMenu } from './DrawerMenu/DrawerMenu';
 export { default as Footer } from './Footer/Footer';
 
