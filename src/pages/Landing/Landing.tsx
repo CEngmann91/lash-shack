8865c9b88a7124c3828598c9e5547a7f03bae99e
CEngmann91 <christian.j.engmann@googlemail.com>
2024-05-17 09:53:35 +0100

    Lazy loading landing page

--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -1,3 +1,4 @@
+import React, { useCallback, memo } from 'react';
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 // Import Swiper styles
@@ -9,20 +10,33 @@ import "swiper/scss/pagination";
 import { EffectCoverflow, Autoplay } from "swiper";
 
 import './Landing.scss';
-import { ArrowMotionButton, LimitedTimeOffer, PageWrapper, SwiperCarousel, TextReflection } from '../../components'
+import { ArrowMotionButton, LimitedTimeOffer, PageWrapper, SwiperCarousel } from '../../components'
 import { Container, Row, Col } from 'reactstrap'
 import images from '../../res/images';
-import { SkeletonImage } from '../../components'
+// import { SkeletonImage } from '../../components'
 import useGetMiscellaneous from "../../hooks/useMiscellaneous";
-import { About, MeetExperts, Testimonials, FindUs } from "..";
+// import { About, MeetExperts, Testimonials, FindUs } from "..";
 import { launchTreatwell } from "../../util/util";
-import Parallax from "../../components/Parallax/Parallax";
-import { useEffect } from "react";
-import { CONTACT } from "../../constants/constants";
+// import Parallax from "../../components/Parallax/Parallax";
+// import { useEffect } from "react";
+// import { CONTACT } from "../../constants/constants";
 
 const Landing = () => {
   const { landingPage_LimitedTimOffer, loadingMiscellaneous, miscellaneousError } = useGetMiscellaneous();
   const list = ["Eyelashes", "Brows", "Aesthetics", "Training"];
+
+  const launchTreatwellCallback = useCallback(() => {
+    launchTreatwell('Romford');
+  }, []);
+
+  const renderServiceItem = useCallback((item: string, index: number) => (
+    <span className="app__border-bottom" key={item}>{item}</span>
+  ), []);
+
+  const AboutLazy = React.lazy(() => import("../About/About"));
+  const MeetExpertsLazy = React.lazy(() => import("../MeetExperts/MeetExperts"));
+  const TestimonialsLazy = React.lazy(() => import("../Testimonials/Testimonials"));
+  const FindUsLazy = React.lazy(() => import("../FindUs/FindUs"));
 
 
   const carousel = () => {
@@ -38,51 +52,6 @@ const Landing = () => {
     );
   }
 
-
-  // const carousel = () => (
-  //   <Swiper
-  //     modules={[EffectCoverflow, Autoplay]}
-  //     centeredSlides={true}
-  //     loop={true}
-  //     effect='coverflow'
-  //     speed={1000}
-  //     slideToClickedSlide={false}
-  //     slidesPerView="auto"
-  //     coverflowEffect={{
-  //       rotate: 30,
-  //       slideShadows: false,
-  //       scale: 1,
-  //       depth: 150,
-  //     }}
-  //     autoplay={{
-  //       delay: 5000,
-  //       pauseOnMouseEnter: true,
-  //       disableOnInteraction: false,
-  //     }}
-  //   >
-  //     <SwiperSlide>
-  //       <SkeletonImage src={images.Landing0} alt="" className="" />
-  //       {/* <img src={images.Landing0} alt="" /> */}
-  //     </SwiperSlide>
-
-  //     <SwiperSlide>
-  //       <SkeletonImage src={images.Landing1} alt="" className="" />
-  //       {/* <img src={images.Landing1} alt="" /> */}
-  //     </SwiperSlide>
-
-  //     <SwiperSlide>
-  //       <SkeletonImage src={images.Landing2} alt="" className="" />
-  //       {/* <img src={images.Landing2} alt="" /> */}
-  //     </SwiperSlide>
-
-  //     <SwiperSlide>
-  //       <SkeletonImage src={images.Landing3} alt="" className="" />
-  //       {/* <img src={images.Landing3} alt="" /> */}
-  //     </SwiperSlide>
-
-  //     <div className="swiper-gradient" />
-  //   </Swiper>
-  // )
 
   const renderLimitedTimeOffer = () => {
     const { active, content, background, textColour, devMode } = landingPage_LimitedTimOffer;
@@ -127,11 +96,9 @@ const Landing = () => {
                 <h1 className="landing__title">New <span>Beginnings</span> Start <span>Here</span></h1>
                 <p>Experienced Technicians, Offering A World Class Service</p>
                 <div className="landing__services">
-                  {list.map((item, index) => (
-                    <span className="app__border-bottom" key={index}>{item}</span>
-                  ))}
+                  {list.map(renderServiceItem)}
                 </div>
-                <ArrowMotionButton className='landing__cta-button' onClick={() => launchTreatwell('Romford')}>
+                <ArrowMotionButton className='landing__cta-button' onClick={launchTreatwellCallback}>
                   Shop Now
                 </ArrowMotionButton>
               </div>
@@ -144,19 +111,16 @@ const Landing = () => {
         </Container>
       </section>
 
-
-
       {renderLimitedTimeOffer()}
 
-      <About />
-
-      <MeetExperts />
-
-      <Testimonials />
-
-      <FindUs />
+      <React.Suspense fallback={<div>Loading...</div>}>
+        <AboutLazy />
+        <MeetExpertsLazy />
+        <TestimonialsLazy />
+        <FindUsLazy />
+      </React.Suspense>
     </PageWrapper>
   )
 }
 
-export default Landing
+export default memo(Landing)
