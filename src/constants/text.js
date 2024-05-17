15fcf6774709f1ae5cf11970050fd450e9bb46ea
CEngmann91 <christian.j.engmann@googlemail.com>
2024-05-17 10:39:47 +0100

    Update Refund Policy formatting

---
+++ src/constants/text.js
@@ -0,0 +1,7 @@
+export const REFUND_POLICY_TEXT = {
+    TITLE: "Can I have a refund?",
+    PARAGRAPH_1: "We take pride in providing excellent service to our established and future clientele, and our policies are presented and provided to reflect this commitment.",
+    PARAGRAPH_2: "Please note that we do not issue refunds on salon services. However, if you have any questions or concerns about the service you received, please notify the salon within two days of receiving it. If you are not satisfied with the work performed, we require that you return to the salon so that your hair can be visually inspected. At that time, we will make every effort to make any adjustments to your satisfaction by rescheduling you with the stylist who performed the initial service. Adjustments are made available to you, but only within 3 days of the initial service.",
+    HELP_TITLE: "Need More Help?",
+    CONTACT_US: "Contact us"
+};
--- src/pages/RefundPolicy/RefundPolicy.tsx
+++ src/pages/RefundPolicy/RefundPolicy.tsx
@@ -1,26 +1,31 @@
 import './RefundPolicy.scss'
 import { useNavigate } from 'react-router-dom';
+import React, { useCallback } from 'react';
 import { ArrowMotionButton, ImageBanner, PageWrapper } from '../../components'
+import { REFUND_POLICY_TEXT } from '../../constants/text';
 
 const RefundPolicy = () => {
     const navigate = useNavigate();
 
+    const handleContactClick = useCallback(() => {
+        navigate("/contact");
+    }, [navigate]);
+
     return (
         <PageWrapper title="Refunds">
             <ImageBanner title='Refund Policy' />
-            
+
             <section className='refund__section'>
+                <h1 className="text-center mb-4">{REFUND_POLICY_TEXT.TITLE}</h1>
 
-                <h1 className="text-center mb-4">Can I have a refund?</h1>
-
-                <p className="px-4 text-center">We take pride in providing excellent service to our established and future clientele, and our policies are presented and provided to reflect this commitment.</p>
-                <br/>
-                <p className="px-4 text-center"><b>Please note that we do not issue refunds on salon services.</b> However, if you have any questions or concerns about the service you received, please notify the salon within two days of receiving it. If you are not satisfied with the work performed, we require that you return to the salon so that your hair can be visually inspected. At that time, we will make every effort to make any adjustments to your satisfaction by rescheduling you with the stylist who performed the initial service. Adjustments are made available to you, but only within <b>3</b> days of the initial service.</p>
+                <p className="px-4 text-center">{REFUND_POLICY_TEXT.PARAGRAPH_1}</p>
+                <br />
+                <p className="px-4 text-center">{REFUND_POLICY_TEXT.PARAGRAPH_2}</p>
 
                 <div className='d-flex flex-column justify-content-center align-items-center mt-4'>
-                    <h4 className='text-center'>Need More Help?</h4>
-                    <ArrowMotionButton className='refund__cta-button w-15 mt-2' onClick={() => navigate("/contact")}>
-                        Contact us
+                    <h4 className='text-center'>{REFUND_POLICY_TEXT.HELP_TITLE}</h4>
+                    <ArrowMotionButton className='refund__cta-button w-15 mt-2' onClick={handleContactClick}>
+                        {REFUND_POLICY_TEXT.CONTACT_US}
                     </ArrowMotionButton>
                 </div>
             </section>
@@ -28,4 +33,4 @@ const RefundPolicy = () => {
     )
 }
 
-export default RefundPolicy
+export default React.memo(RefundPolicy);
