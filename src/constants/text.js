7398ce4e79f7c7869b0837580f9666fbee1a5454
CEngmann91 <christian.j.engmann@googlemail.com>
2024-06-29 09:23:09 +0100

    Policies const

--- src/constants/text.js
+++ src/constants/text.js
@@ -16,10 +16,21 @@ export const BASKET_TEXT = {
     CHECKOUT: "Checkout"
 };
 
-export const REFUND_POLICY_TEXT = {
-    TITLE: "Can I have a refund?",
-    PARAGRAPH_1: "We take pride in providing excellent service to our established and future clientele, and our policies are presented and provided to reflect this commitment.",
-    PARAGRAPH_2: "Please note that we do not issue refunds on salon services. However, if you have any questions or concerns about the service you received, please notify the salon within two days of receiving it. If you are not satisfied with the work performed, we require that you return to the salon so that your hair can be visually inspected. At that time, we will make every effort to make any adjustments to your satisfaction by rescheduling you with the stylist who performed the initial service. Adjustments are made available to you, but only within 3 days of the initial service.",
-    HELP_TITLE: "Need More Help?",
-    CONTACT_US: "Contact us"
+export const POLICIES = {
+    APPOINTMENTS: {
+        TITLE: "Book An Appointment",
+        DESCRIPTION: "All appointments require a £10 booking fee. This is non-refundable and is non-transferable."
+    },
+    LATENESS: {
+        TITLE: "Lateness",
+        DESCRIPTION: "If you are more than 15 minutes late for your scheduled appointment without prior notice, your appointment will be cancelled and a cancellation fee charged."
+    },
+    CANCELLATIONS: {
+        TITLE: "Cancellations",
+        DESCRIPTION: "Cancelling your appointment with less than 24 hours notice will incur an extra cancellation fee. Not turning up for your appointment without prior notice will require all appointments ot be paid up front in the future."
+    },
+    REFUND: {
+        TITLE: "Refund Policy",
+        DESCRIPTION: "We take pride in providing excellent service to our established and future clientele, and our policies are presented and provided to reflect this commitment.\nPlease note that we do not issue refunds on salon services. However, if you have any questions or concerns about the service you received, please notify the salon within two days of receiving it. If you are not satisfied with the work performed, we require that you return to the salon so that your hair can be visually inspected.\nAt that time, we will make every effort to make any adjustments to your satisfaction by rescheduling you with the stylist who performed the initial service. Adjustments are made available to you, but only within 3 days of the initial service."
+    },
 };
