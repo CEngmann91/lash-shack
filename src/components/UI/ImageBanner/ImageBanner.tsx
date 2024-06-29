d0cb97e757b4dacf53927a4972e6b599c8ebadc4
CEngmann91 <christian.j.engmann@googlemail.com>
2024-06-29 09:14:32 +0100

    Added subtitle back

--- src/components/UI/ImageBanner/ImageBanner.tsx
+++ src/components/UI/ImageBanner/ImageBanner.tsx
@@ -2,14 +2,14 @@ import './ImageBanner.scss';
 
 type ImageBannerProps = {
     title: string;
-    subtitle?: string;
+    subtitle?: string | undefined;
 }
-const ImageBanner = ({ title, subtitle = "" }: ImageBannerProps) => {
+const ImageBanner = ({ title, subtitle }: ImageBannerProps) => {
     
     return (
         <section className="image__banner">
             <h1 className="text__neon-pink">{title}</h1>
-            {/* <h6>{subtitle}</h6> */}
+            {subtitle && <h6 className="text__neon-pink">{subtitle}</h6>}
         </section>
     )
 }
