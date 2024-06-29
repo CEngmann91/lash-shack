0121a58715d0844f4259e033317558f341c97801
CEngmann91 <christian.j.engmann@googlemail.com>
2024-06-29 09:25:38 +0100

    Updated Limited time offer and clock

--- src/components/UI/LimitedTimeOffer/LimitedTimeOffer.tsx
+++ src/components/UI/LimitedTimeOffer/LimitedTimeOffer.tsx
@@ -21,6 +21,7 @@ type LimitedTimeOfferProps = {
     buttonText?: string;
     buttonTextColour?: string;
     buttonURL?: string;
+    noCountdown?: boolean;
 }
 
 const LimitedTimeOffer = React.memo(({
@@ -36,51 +37,60 @@ const LimitedTimeOffer = React.memo(({
     buttonBG = "#ec439f",
     buttonText,
     buttonTextColour = "#fff",
-    buttonURL
+    buttonURL,
+    noCountdown
 }: LimitedTimeOfferProps) => {
     const navigate = useNavigate();
     const [now, setNow] = useState(new Date().getTime());
 
     useEffect(() => {
+        if (noCountdown) {
+            return;
+        }
+
         const interval = setInterval(() => {
             setNow(new Date().getTime());
         }, 1000);
         return () => clearInterval(interval);
-    }, []);
+    }, [noCountdown]);
 
-    const start = new Date(startDate).getTime();
-    const isInFuture = start - now;
-    if (isInFuture > 0)
-        return null
-
-    const destination = new Date(endDate).getTime();
-    const dif = destination - now;
-    if (dif < 0)
-        return null
+    if (!noCountdown) {
+        const start = new Date(startDate).getTime();
+        const isInFuture = start - now;
+        if (isInFuture > 0)
+            return null
+    
+        const destination = new Date(endDate).getTime();
+        const dif = destination - now;
+        if (dif < 0)
+            return null
+    }
 
     const handleClick = () => {
         buttonURL ? openWindow(buttonURL) : launchTreatwell(location)
     }
 
     return (
-        <section className="timer__wrapper" style={{ background: background }}>
+        <section className={`${background?.includes('url') && 'tint-background'} timer__wrapper`} style={{ background: background }}>
             <Container>
                 <Row>
                     <Col lg='6' md='12' className='countdown-col'>
                         <div className="clock__top-content">
-                            <h4 className={`fs-6 mb-2`} style={{ color: textColour }}>{subtitle}</h4>
+                            {subtitle && <h4 className={`fs-6 mb-2`} style={{ color: textColour }}>{subtitle}</h4>}
                             <h3 className={`fs-5 mb-3`} style={{ color: textColour }}>{title}</h3>
                         </div>
-                        <Clock destinationDate={endDate} textColour={textColour} onTimerCompleted={onTimerCompleted} />
+                        {!noCountdown && <Clock destinationDate={endDate} textColour={textColour} onTimerCompleted={onTimerCompleted} /> }
 
                         <ArrowMotionButton className='store__button' style={{ background: buttonBG, color: buttonTextColour }} onClick={handleClick}>
                             {buttonText}
                         </ArrowMotionButton>
                     </Col>
 
-                    <Col lg='4' md='12' className='text-end counter__image'>
-                        <img src={imageUrl ?? images.LogoNoBG} alt="" />
-                    </Col>
+                    {imageUrl && (
+                        <Col lg='4' md='12' className='text-end counter__image'>
+                            <img src={imageUrl ?? images.LogoNoBG} alt="" />
+                        </Col>
+                    )}
                 </Row>
             </Container>
         </section>
