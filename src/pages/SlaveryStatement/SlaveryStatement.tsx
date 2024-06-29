01f5013eb5548d0c1a2c4694992344a0389fea8a
CEngmann91 <christian.j.engmann@googlemail.com>
2024-06-29 09:20:31 +0100

    Added basis for Slavery statement (Legal)

---
+++ src/pages/SlaveryStatement/SlaveryStatement.tsx
@@ -0,0 +1,15 @@
+import './SlaveryStatement.scss';
+import { ImageBanner, PageWrapper } from '../../components';
+
+const SlaveryStatement = () => {
+    return (
+        <PageWrapper title="Slavery Statement">
+            <ImageBanner title='Modern Slavery Statement' />
+
+            <section className='statement__section'>
+            </section>
+        </PageWrapper>
+    )
+}
+
+export default SlaveryStatement
